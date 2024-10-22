387f95938aa19d1455053e7b50e4382162f15701
Antonio Huinac <huinacsam0@gmail.com>
2024-10-21 21:42:33 -0600

    inicio de comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import {AsyncPipe} from '@angular/common';
+
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ModalModule.forRoot(),
+    ToastrModule.forRoot(),
+    NgMultiSelectDropDownModule.forRoot(),
+    TimepickerModule.forRoot(),
+    AsyncPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
